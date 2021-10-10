36aa66be9ddf3d91e77dcab3bc481e12133a686f
Chris Dosé <chris.dose@gmail.com>
2021-10-10 15:52:31 -0700

    handle api errors slightly better

--- api/ambry.js
+++ api/ambry.js
@@ -1,28 +1,33 @@
 const BASE_URL = `http://192.168.0.8:4000`
 const API_URL = `${BASE_URL}/api`
 
+async function handleResponse (response) {
+  if (response.ok) {
+    const result = await response.json()
+    return result.data
+  } else {
+    return Promise.reject(response)
+  }
+}
+
 export async function getRecentBooks (page = 1) {
   const response = await fetch(`${API_URL}/books?page=${page}`)
-  const result = await response.json()
-  return result.data
+  return handleResponse(response)
 }
 
 export async function getBook (bookId) {
   const response = await fetch(`${API_URL}/books/${bookId}`)
-  const result = await response.json()
-  return result.data
+  return handleResponse(response)
 }
 
 export async function getPerson (personId) {
   const response = await fetch(`${API_URL}/people/${personId}`)
-  const result = await response.json()
-  return result.data
+  return handleResponse(response)
 }
 
 export async function getSeries (seriesId) {
   const response = await fetch(`${API_URL}/series/${seriesId}`)
-  const result = await response.json()
-  return result.data
+  return handleResponse(response)
 }
 
 export function formatImageUri (path) {
