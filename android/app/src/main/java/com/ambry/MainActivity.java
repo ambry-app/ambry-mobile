76fec76544631cbc3c3b840873d4e739b7132d90
Chris Dosé <chris.dose@gmail.com>
2022-06-16 22:19:39 -0700

    RN 0.67 -> 0.68

--- android/app/src/main/java/com/ambry/MainActivity.java
+++ android/app/src/main/java/com/ambry/MainActivity.java
@@ -1,9 +1,9 @@
 package com.ambry;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
 import com.zoontek.rnbootsplash.RNBootSplash;
 
 import io.moox.rntransparentstatusandnavigationbar.RNTransparentStatusAndNavigationBar;
@@ -26,13 +26,26 @@ public class MainActivity extends ReactActivity {
     RNTransparentStatusAndNavigationBar.init(MainActivity.this);
   }
 
-  // Fix for react-native Appearance API not reacting to system changes.
-  // TODO: This can be removed once we upgrade to a version of react-native that
-  // includes this commit:
-  // https://github.com/facebook/react-native/commit/25a2c608f790f42cbc4bb0a90fc06cc7bbbc9b95
+  /**
+   * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
+   * you can specify the rendered you wish to use (Fabric or the older renderer).
+   */
   @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    getReactInstanceManager().onConfigurationChanged(this, newConfig);
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new MainActivityDelegate(this, getMainComponentName());
+  }
+
+  public static class MainActivityDelegate extends ReactActivityDelegate {
+    public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
+      super(activity, mainComponentName);
+    }
+
+    @Override
+    protected ReactRootView createRootView() {
+      ReactRootView reactRootView = new ReactRootView(getContext());
+      // If you opted-in for the New Architecture, we enable the Fabric Renderer.
+      reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
+      return reactRootView;
+    }
   }
 }
