5c67786d2c37cfb6b8594f6fcc2a6de10638638d
Chris Dosé <chris.dose@gmail.com>
2022-06-21 00:41:31 -0700

    Remove sleep timer fade
    RN 0.68 has a bug where setTimeout and setInterval do not work in
    headless js tasks. We can put this back once we upgrade to 0.69

--- src/tasks/SleepTimerTask.js
+++ src/tasks/SleepTimerTask.js
@@ -1,41 +1,15 @@
-import TrackPlayer from 'react-native-track-player'
 import { pause } from '../stores/Player'
-import { FADE_TIME, useSleepTimer } from '../stores/SleepTimer'
-
-const INTERVAL = 500 // milliseconds
-const STEPS = (1000 / INTERVAL) * FADE_TIME
+import { useSleepTimer } from '../stores/SleepTimer'
 
 export default async function run() {
-  console.debug('SleepTimerTask: sleep timer fade started')
+  console.debug('SleepTimerTask: sleep timer triggered')
 
-  const { targetTime } = useSleepTimer.getState()
+  const timer = useSleepTimer.getState()
 
-  return new Promise((resolve, reject) => {
-    let currentStep = STEPS
+  if (!timer.enabled) {
+    console.debug('SleepTimerTask: canceled')
+    return
+  }
 
-    const interval = setInterval(async () => {
-      const timer = useSleepTimer.getState()
-
-      // if the timer is no longer enabled or the target time has changed since
-      // we started running
-      if (!timer.enabled || timer.targetTime !== targetTime) {
-        console.debug('SleepTimerTask: fade canceled')
-
-        await TrackPlayer.setVolume(1)
-        clearInterval(interval)
-        return resolve()
-      }
-
-      if (currentStep > 0) {
-        TrackPlayer.setVolume(currentStep / STEPS)
-        currentStep -= 1
-      } else {
-        console.debug('SleepTimerTask: fade finished')
-
-        clearInterval(interval)
-        await Promise.all([pause(), TrackPlayer.setVolume(1)])
-        resolve()
-      }
-    }, INTERVAL)
-  })
+  await pause()
 }
