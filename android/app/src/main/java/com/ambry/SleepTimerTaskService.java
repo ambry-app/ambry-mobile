a05d529b33a5278543ce547a93418945e76a9bb8
Chris Dosé <chris.dose@gmail.com>
2022-06-21 00:35:06 -0700

    Implement broadcast receiver for sleep timer

--- android/app/src/main/java/com/ambry/SleepTimerTaskService.java
+++ android/app/src/main/java/com/ambry/SleepTimerTaskService.java
@@ -2,17 +2,25 @@ package com.ambry;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+
 import javax.annotation.Nullable;
 
-public class SleepTimerService extends HeadlessJsTaskService {
+public class SleepTimerTaskService extends HeadlessJsTaskService {
 
     @Override
-    protected @Nullable HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+    protected @Nullable
+    HeadlessJsTaskConfig getTaskConfig(Intent intent) {
+        Log.d("SleepTimerTaskService", "getTaskConfig called");
+
         Bundle extras = intent.getExtras();
         if (extras != null) {
+            Log.d("SleepTimerTaskService", "returned task config");
+
             return new HeadlessJsTaskConfig(
                     "SleepTimerTask",
                     Arguments.fromBundle(extras),
@@ -20,7 +28,8 @@ public class SleepTimerService extends HeadlessJsTaskService {
                     true // optional: defines whether or not  the task is allowed in foreground. Default is false
             );
         }
+
+        Log.d("SleepTimerTaskService", "returned null!");
         return null;
     }
-
 }
