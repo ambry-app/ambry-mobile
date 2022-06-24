30180cbf2712a7fff5dc1bc980150e6bbf3059c6
Chris Dosé <chris.dose@gmail.com>
2022-06-23 23:54:18 -0700

    New bootsplash

--- generate_bootsplash.sh
+++ generate_bootsplash.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 yarn react-native generate-bootsplash assets/icon_with_shadow.png \
-  --background-color=84CC16 \
-  --logo-width=100 \
+  --background-color=27272A \
+  --logo-width=150 \
   --assets-path=assets \
   --flavor=main
 
-yarn react-native generate-bootsplash assets/icon_with_shadow.png \
-  --background-color=F59E0B \
-  --logo-width=100 \
+yarn react-native generate-bootsplash assets/icon_with_shadow_debug.png \
+  --background-color=27272A \
+  --logo-width=150 \
   --assets-path=assets \
-  --flavor=main
+  --flavor=debug
