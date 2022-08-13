53fcf482b93a294a5192b424703db0a54c6c7cb1
Chris Dosé <chris@xn--dos-dma.com>
2022-08-13 01:06:27 -0700

    Sign in screen redesign

--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,15 +1,14 @@
-import { Picker } from '@react-native-picker/picker'
 import React, { useEffect, useState } from 'react'
-import { Button, Text, useColorScheme, View } from 'react-native'
-import {
-  NativeViewGestureHandler,
-  ScrollView,
-  TextInput
-} from 'react-native-gesture-handler'
-import Logo from '../assets/logo_256x1056.svg'
+import { Button, Text, View } from 'react-native'
+import DropDownPicker from 'react-native-dropdown-picker'
+import { ScrollView, TextInput } from 'react-native-gesture-handler'
+import Logo from '../assets/logo.svg'
 import LargeActivityIndicator from '../components/LargeActivityIndicator'
 import tw from '../lib/tailwind'
 import useAmbryAPI, { useLoginAction } from '../stores/AmbryAPI'
+
+DropDownPicker.setTheme('DARK')
+DropDownPicker.setListMode('SCROLLVIEW')
 
 export default function SignInScreen() {
   const knownHosts = useAmbryAPI(state => state.knownHosts)
@@ -17,7 +16,6 @@ export default function SignInScreen() {
   const [email, setEmail] = useState('')
   const [host, setHost] = useState('')
   const [password, setPassword] = useState('')
-  const scheme = useColorScheme()
 
   useEffect(() => {
     if (knownHosts[0]) {
@@ -25,69 +23,55 @@ export default function SignInScreen() {
     }
   }, [knownHosts])
 
+  useEffect(() => {
+    if (host === 'new') {
+      setShowHostInput(true)
+      setHost('')
+    }
+  }, [host])
+
   const { isLoading, isError, login } = useLoginAction(host, email, password)
+
+  const [hostPickerOpen, setHostPickerOpen] = useState(false)
+  const [hostPickerItems, setHostPickerItems] = useState(
+    knownHosts
+      .map(selectableHost => ({
+        label: selectableHost,
+        value: selectableHost
+      }))
+      .concat([{ label: 'New Host', value: 'new' }])
+  )
 
   return (
     <ScrollView style={tw`p-4`}>
       <View style={tw`py-8 items-center`}>
-        <Logo
-          height="86"
-          width="352"
-          brandColor={
-            scheme === 'dark' ? tw.color('lime-400') : tw.color('lime-500')
-          }
-          textColor={
-            scheme === 'dark' ? tw.color('gray-200') : tw.color('gray-700')
-          }
-        />
-        <Text
-          style={tw`text-lg font-semibold text-gray-500 dark:text-gray-400`}
-        >
+        <Logo height="64" />
+        <Text style={tw`text-lg font-semibold text-gray-400`}>
           Personal Audiobook Streaming
         </Text>
       </View>
+
       {knownHosts.length === 0 || showHostInput ? (
         <TextInput
           placeholder="Host"
           value={host}
           autoCapitalize="none"
           onChangeText={setHost}
-          style={tw`my-2 text-gray-700 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-500 bg-white dark:bg-gray-700 rounded px-3 py-2`}
-          placeholderTextColor={
-            scheme === 'dark' ? tw.color('gray-500') : tw.color('gray-300')
-          }
+          style={tw`my-2 text-gray-200 bg-gray-800 rounded px-3 py-2`}
+          placeholderTextColor={tw.color('gray-500')}
         />
       ) : (
-        <View
-          style={tw`my-2 border-2 border-gray-300 dark:border-gray-500 bg-white dark:bg-gray-700 rounded`}
-        >
-          <NativeViewGestureHandler>
-            <Picker
-              selectedValue={host}
-              onValueChange={(itemValue, _itemIndex) =>
-                itemValue === 'new'
-                  ? (() => {
-                      setShowHostInput(true)
-                      setHost('')
-                    })()
-                  : setHost(itemValue)
-              }
-              style={tw`text-gray-700 dark:text-gray-200`}
-              dropdownIconColor={
-                scheme === 'dark' ? tw.color('gray-200') : tw.color('gray-700')
-              }
-            >
-              {knownHosts.map(selectableHost => (
-                <Picker.Item
-                  key={selectableHost}
-                  label={selectableHost}
-                  value={selectableHost}
-                />
-              ))}
-              <Picker.Item label="New Host" value="new" />
-            </Picker>
-          </NativeViewGestureHandler>
-        </View>
+        <DropDownPicker
+          open={hostPickerOpen}
+          value={host}
+          items={hostPickerItems}
+          setOpen={setHostPickerOpen}
+          setValue={setHost}
+          setItems={setHostPickerItems}
+          style={tw`bg-gray-800 border-0 rounded my-2`}
+          textStyle={tw`text-gray-200`}
+          listItemContainerStyle={tw`bg-gray-900`}
+        />
       )}
       <TextInput
         placeholder="Email"
@@ -97,20 +81,16 @@ export default function SignInScreen() {
         textContentType="emailAddress"
         autoCompleteType="email"
         keyboardType="email-address"
-        style={tw`my-2 text-gray-700 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-500 bg-white dark:bg-gray-700 rounded px-3 py-2`}
-        placeholderTextColor={
-          scheme === 'dark' ? tw.color('gray-500') : tw.color('gray-300')
-        }
+        style={tw`my-2 text-gray-200 bg-gray-800 rounded px-3 py-2`}
+        placeholderTextColor={tw.color('gray-500')}
       />
       <TextInput
         placeholder="Password"
         value={password}
         onChangeText={setPassword}
         secureTextEntry
-        style={tw`my-2 mb-4 text-gray-700 dark:text-gray-200 border-2 border-gray-300 dark:border-gray-500 bg-white dark:bg-gray-700 rounded px-3 py-2`}
-        placeholderTextColor={
-          scheme === 'dark' ? tw.color('gray-500') : tw.color('gray-300')
-        }
+        style={tw`my-2 mb-4 text-gray-200 bg-gray-800 rounded px-3 py-2`}
+        placeholderTextColor={tw.color('gray-500')}
       />
       <Button
         title="Sign in"
